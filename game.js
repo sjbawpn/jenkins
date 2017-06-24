9ac9e98712176bf05a6800a792f3df2440dd342f
Sofiane Djeffal <sjbawpn@live.com>
2017-06-24 16:59:47 -0400

    Added help and create game template. Cleaned up code

--- game.js
+++ game.js
@@ -13,5 +13,27 @@ module.exports = {
 
         });
 
+    },
+
+    help: function () {
+
+        return new Promise(function (resolve) {
+            message = "Try asking to join a game :-)";
+
+            // complete promise with a timer to simulate async response
+            setTimeout(function () { resolve(message); }, 1000);
+
+        });
+    },
+
+    createGame: function (gameName) {
+
+        return new Promise(function (resolve) {
+            message = util.format("I'd love to help you create \"%s\" but I'm not ready to help you yet! Come back soon!", gameName);
+
+            // complete promise with a timer to simulate async response
+            setTimeout(function () { resolve(message); }, 1000);
+
+        });
     }
 }
